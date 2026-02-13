61dfbfb637d80ab412d974bc49e4ed2b2d899ea8
Michael Moheban <MMoheban@isotope.com>
2026-02-13 11:21:51 -0500

    v3.0.6: Updated version string

--- GL-260 Data Analysis and Plotter.py
+++ GL-260 Data Analysis and Plotter.py
@@ -1,5 +1,5 @@
 # GL-260 Data Analysis and Plotter
-# Version: v3.0.5
+# Version: v3.0.6
 # Date: 2026-02-13
 
 import os
@@ -9726,7 +9726,7 @@ class AnnotationsPanel:
 
 EXPORT_DPI = 1200
 
-APP_VERSION = "v3.0.5"
+APP_VERSION = "v3.0.6"
 
 DEBUG_LOGGER_NAME = "gl260"
 DEBUG_LOG_FILE = "gl260_debug.log"
